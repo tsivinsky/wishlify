ae41772aaac9ed7385d2634db1e23e4944f19441
Daniil <daniil@tsivinsky.com>
2020-11-20 11:00:10 +0300

    Add functions for hashing passwords to user schema

--- server/models/schemas/user.js
+++ server/models/schemas/user.js
@@ -1,5 +1,6 @@
 // Require dependencies
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 // User schema
 const schema = new mongoose.Schema(
@@ -15,10 +16,24 @@ const schema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      minlength: 8,
     },
   },
   { timestamps: true }
 );
+
+schema.methods.hashPassword = function () {
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(this.password, salt);
+
+  this.password = hash;
+};
+
+schema.methods.checkPassword = function (password = "") {
+  const isMatch = bcrypt.compareSync(password, this.password);
+
+  return isMatch;
+};
 
 // Export user schema
 module.exports = schema;
