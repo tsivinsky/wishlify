684294d3cad01a7915182d0f221049171c3328ee
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-03-25 23:20:02 +0300

    Remove AuthProxy component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,3 @@
-export * from "./AuthProxy";
 export * from "./Header";
 export * from "./Notification";
 export * from "./Wishlist";
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,33 @@
+import { useEffect } from "react";
 import { AppProps } from "next/app";
-import { AuthProxy, Header, Notification } from "../components";
+import { Header, Notification } from "../components";
+import { api } from "../helpers";
+import { useAuth, useLoading, useMessage, useTokenStore } from "../store";
 
 import "../scss/style.scss";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const { auth, setAuth } = useAuth();
+  const { setMessage } = useMessage();
+  const { token } = useTokenStore();
+  const { stopLoading } = useLoading();
+
+  // Get user's data if token exists in localStorage
+  useEffect(() => {
+    if (token && !auth.user) {
+      api.user
+        .getAuthorizedUser(token)
+        .then((user) => setAuth(token, user))
+        .catch((err) => setMessage(err))
+        .then(() => stopLoading());
+    }
+  }, []);
+
   return (
-    <AuthProxy>
+    <>
       <Header router={router} />
       <Notification />
       <Component router={router} {...pageProps} />
-    </AuthProxy>
+    </>
   );
 }
