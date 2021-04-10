0f77e8e538ae6474488964f095fab14b9b2ea377
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-04-10 10:08:28 +0300

    Add error handling controller

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { authRouter, userRouter, wishlistsRouter } from "./routes";
+import { errorController } from "./controllers";
 
 export const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
 router.use("/wishlists", wishlistsRouter);
+
+router.use(errorController.handleErrors);
