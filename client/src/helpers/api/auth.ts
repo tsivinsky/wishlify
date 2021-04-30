741c82322913229e9555c95f6a6f723f809cc17e
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-04-30 14:00:28 +0300

    Create helper function for sending authentication request

--- client/src/helpers/api/auth.ts
+++ client/src/helpers/api/auth.ts
@@ -1,43 +1,13 @@
 import { axios } from "../axiosInstance";
-import { IUser } from "../../types";
 
-interface LoginProps {
-  email: string;
-  password: string;
+interface SigninResponse {
+  message: string;
 }
 
-interface Response {
-  token: string;
-  user: IUser;
-}
-
-export async function login(data: LoginProps): Promise<Response> {
+export async function signin(data: { email: string }): Promise<SigninResponse> {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await axios.post("/auth/login", data);
-
-      resolve(response.data);
-    } catch (err) {
-      if (err.response) {
-        reject(err.response.data);
-      }
-
-      reject(err.message);
-    }
-  });
-}
-
-interface RegisterProps {
-  name: string;
-  email: string;
-  username: string;
-  password: string;
-}
-
-export async function register(data: RegisterProps): Promise<Response> {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await axios.post("/auth/register", data);
+      const response = await axios.post("/auth/signin", data);
 
       resolve(response.data);
     } catch (err) {
