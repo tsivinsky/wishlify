7d2ded95a134deb80f10ac4f4be23bf9350d5f38
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-04-30 11:35:03 +0300

    Change if check

--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -4,7 +4,7 @@ import { useMessage } from "../store";
 export const Notification: React.FC = () => {
   const { message } = useMessage();
 
-  if (message.text !== "") {
+  if (message) {
     return (
       <div className="notification">
         <h2>{message.text}</h2>
