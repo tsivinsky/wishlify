65ec4bc94095e1786874dd7e0ebf6acac510d51c
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-04-30 13:56:53 +0300

    Remove session

--- server/src/types/env.d.ts
+++ server/src/types/env.d.ts
@@ -4,9 +4,9 @@ declare global {
       NODE_ENV: "production" | "development";
       MONGO_URI: string;
       JWT_SECRET: string;
-      SESSION_SECRET: string;
       EMAIL_USER: string;
       EMAIL_PASS: string;
+      CLIENT_URL: string;
     }
   }
 }
