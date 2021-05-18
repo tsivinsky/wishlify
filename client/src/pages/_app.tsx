333e3fb8b774fdad4ebd79024f0b466281f59568
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-05-18 17:58:10 +0300

    Remove notification component when route changes

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -14,6 +14,10 @@ export default function App({ Component, pageProps, router }: AppProps) {
     }
   }, [message]);
 
+  useEffect(() => {
+    removeMessage();
+  }, [router.route]);
+
   return (
     <AuthProxy>
       <Notification />
