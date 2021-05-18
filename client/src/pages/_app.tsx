e25b41993dfe851691ec53678b6c9e5d7d0a340f
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-05-18 17:45:42 +0300

    Add styles for notification component

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 import { Notification, AuthProxy } from "../components";
+import { useMessage } from "../store";
 
 import "../scss/style.scss";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const { message, removeMessage } = useMessage();
+
+  useEffect(() => {
+    if (message) {
+      setTimeout(removeMessage, 3000); // Remove notification after 3 seconds
+    }
+  }, [message]);
+
   return (
     <AuthProxy>
       <Notification />
