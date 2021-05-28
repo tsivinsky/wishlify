8619bb62dc86d1d059325307e1ffc225a2015700
Daniil Tsivinsky <daniil@tsivinsky.com>
2021-05-28 16:20:34 +0300

    Add styles for account page

--- client/src/pages/account.tsx
+++ client/src/pages/account.tsx
@@ -9,7 +9,34 @@ export default function Account({ router }: PageProps) {
     <div id="page" className="account-page">
       <Header router={router} />
 
-      <h1>Hello, {user!.username}!</h1>
+      <main>
+        <div>
+          <h2>Your account data</h2>
+
+          <div id="data">
+            <div id="username" className="row">
+              <h3>Username</h3>
+              <span>{user!.username}</span>
+            </div>
+
+            <div id="email" className="row">
+              <h3>Email</h3>
+              <span>{user!.email}</span>
+            </div>
+
+            <div id="creation-date" className="row">
+              <h3>When you signed up</h3>
+              <span>{parseDate(user!.createdAt)}</span>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
+}
+
+function parseDate(time: string) {
+  const date = new Date(time);
+
+  return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 }
